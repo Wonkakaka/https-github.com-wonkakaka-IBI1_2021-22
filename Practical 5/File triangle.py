96d11b1c6ce8f6e687fc2d2efd0619f178bc41f0
Wonkakaka <101771772+Wonkakaka@users.noreply.github.com>
2022-03-24 15:50:34 +0800

    Update File triangle.py

--- Practical 5/File triangle.py
+++ Practical 5/File triangle.py
@@ -4,7 +4,7 @@ sum=0
 counter=1
 # set the formula to make the program run as required.
 while counter<=number:
-  sum=sum+counter
+	sum=sum+counter
 # List result as need
 	print("sum=",sum) 
 # The process is not over yet, all ten results need to be enumerated.
