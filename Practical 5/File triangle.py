ea9a27cc52146ed2e89d1c1b9c92ddb9797545a4
Wonkakaka <101771772+Wonkakaka@users.noreply.github.com>
2022-03-24 10:23:53 +0800

    Create File triangle.py

---
+++ Practical 5/File triangle.py
@@ -0,0 +1,11 @@
+# Setting range and the starting amount.
+number=10
+sum=0
+counter=1
+# set the formula to make the program run as required.
+while counter<=number:
+  sum=sum+counter
+# List result as need
+print("sum=",sum) 
+# The process is not over yet, all ten results need to be enumerated.
+counter+=1
