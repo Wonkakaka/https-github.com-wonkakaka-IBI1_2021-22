b8d34c8d5d2eee2a78a7540672f2382b7dfb73c1
Wonkakaka <101771772+Wonkakaka@users.noreply.github.com>
2022-03-24 15:51:47 +0800

    Update File variables.py

--- Practical 5/File variables.py
+++ Practical 5/File variables.py
@@ -6,6 +6,6 @@ d=b-c
 e=a-b
 # Output the corresponding contents by setting the conditions
 if d>e:
-  print(" the difference between the numbers of cases in 2020 and 2021 is greater")
+	print(" the difference between the numbers of cases in 2020 and 2021 is greater")
 else:
-  print(" the difference between the numbers of cases in 2021 and 2022 is greater")
+	print(" the difference between the numbers of cases in 2021 and 2022 is greater")
