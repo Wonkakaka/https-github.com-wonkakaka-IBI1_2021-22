a20fbae7c383b361b119bc78f74cb8f7062fcf16
Wonkakaka <101771772+Wonkakaka@users.noreply.github.com>
2022-03-24 09:51:05 +0800

    Create File variables.py

---
+++ Practical 5/File variables.py
@@ -0,0 +1,11 @@
+# Use letters to represent data
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+# Output the corresponding contents by setting the conditions
+if d>e:
+  print(" the difference between the numbers of cases in 2020 and 2021 is greater")
+else:
+  print(" the difference between the numbers of cases in 2021 and 2022 is greater")
