6d6102a6cacbdcb9120cbe6bd4979133f7af8bad
Wonkakaka <101771772+Wonkakaka@users.noreply.github.com>
2022-03-24 15:49:35 +0800

    Update Pizza_party.py

--- Practical 5/Pizza_party.py
+++ Practical 5/Pizza_party.py
@@ -1,1 +1,11 @@
-#
+# Set the maximum number of pieces required
+number=64
+# The value of n at the beginning
+n=1
+p=2
+# Ensure that the program does not stop until p reaches 64.
+while p<number:
+        p=(n*n+n+2)/2
+        print("when the pieces of pizza is:",p,"the number of cuts is:",n)
+        n+=1
+
