1e0304628d0783da509f2d0757b9a3c40ac4a549
Wonkakaka <101771772+Wonkakaka@users.noreply.github.com>
2022-05-25 11:19:00 +0800

    Delete fragment_counter.py.py

--- fragment_counter.py.py
+++
@@ -1,4 +0,0 @@
-import re # Importing a Library
-seq = 'ATGCAATCGACTACGATCAATCGAGGGCC'# create a string variable seq
-Find = re.findall(r'GAATTC',seq)
-print("the total number of fragments=",len(Find)+1)
